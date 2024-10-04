bf82f29f7e9624a24ab09e05949f02e51a9b0c4f
Abilasha S. <UT01211@unicomtic.com>
2024-10-04 11:55:29 +0530

    API Update_1

--- BackEnd/BIKERENTALAPI/BIKERENTALAPI/Repository/CustomerRepository.cs
+++ BackEnd/BIKERENTALAPI/BIKERENTALAPI/Repository/CustomerRepository.cs
@@ -89,5 +89,120 @@ namespace BIKERENTALAPI.Repository
         }
 
 
+        public async Task<Rental> GetRentalByID(Guid rentalId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var command = new SqlCommand(
+                    "SELECT * FROM Rentals WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", rentalId);
+
+                var reader = await command.ExecuteReaderAsync(CommandBehavior.SingleRow);
+                if (await reader.ReadAsync())
+                {
+                    return new Rental
+                    {
+                        id = reader.GetGuid(reader.GetOrdinal("Id")),
+                        CustomerID = reader.GetGuid(reader.GetOrdinal("CustomerID")),
+                        MotorbikeID = reader.GetGuid(reader.GetOrdinal("MotorbikeID")),
+                        RentalDate = reader.GetDateTime(reader.GetOrdinal("RentalDate")),
+                        Returndate = reader.GetDateTime(reader.GetOrdinal("Returndate")),
+                        Isoverdue = reader.GetBoolean(reader.GetOrdinal("Isoverdue")),
+                        status = reader.GetString(reader.GetOrdinal("status")),
+                    };
+                }
+                return null;
+            }
+        }
+
+        public async Task<List<Rental>> GetAllRentalsByCustomerID(Guid customerId)
+        {
+            var rentals = new List<Rental>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var command = new SqlCommand(
+                    "SELECT * FROM Rentals WHERE CustomerID = @CustomerID", connection);
+                command.Parameters.AddWithValue("@CustomerID", customerId);
+
+                var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    rentals.Add(new Rental
+                    {
+                        id = reader.GetGuid(reader.GetOrdinal("Id")),
+                        CustomerID = reader.GetGuid(reader.GetOrdinal("CustomerID")),
+                        MotorbikeID = reader.GetGuid(reader.GetOrdinal("MotorbikeID")),
+                        RentalDate = reader.GetDateTime(reader.GetOrdinal("RentalDate")),
+                        Returndate = reader.GetDateTime(reader.GetOrdinal("Returndate")),
+                        Isoverdue = reader.GetBoolean(reader.GetOrdinal("Isoverdue")),
+                        status = reader.GetString(reader.GetOrdinal("status")),
+
+                    });
+                }
+            }
+            return rentals;
+        }
+
+        public async Task<List<Customer>> GetAllCustomer()
+        {
+            var customers = new List<Customer>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var command = new SqlCommand("SELECT * FROM Customers", connection);
+
+                var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    customers.Add(new Customer
+                    {
+                        Id = reader.GetGuid(reader.GetOrdinal("Id")),
+                        FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                        Mobilenumber = reader.GetInt32(reader.GetOrdinal("Mobilenumber")),
+                        Licence = reader.GetInt32(reader.GetOrdinal("Licence")),
+                        Nic = reader.GetInt32(reader.GetOrdinal("Nic")),
+                        Password = reader.GetString(reader.GetOrdinal("Password")),
+                        IsActive = reader.GetBoolean(reader.GetOrdinal("IsActive"))
+
+                    });
+                }
+            }
+            return customers;
+        }
+
+        public async Task<Customer> Addcustomer(Customer customer)
+        {
+
+            if (customer.Id == Guid.Empty)
+            {
+                customer.Id = Guid.NewGuid();
+            }
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var command = new SqlCommand(
+                    "INSERT INTO Customers (Id, FirstName, Mobilenumber, Licence, Nic, Password, IsActive) " +
+                    "VALUES (@Id, @FirstName, @Mobilenumber,@Licence, @Nic, @Password,@IsActive);",
+                    connection);
+
+
+                command.Parameters.AddWithValue("@Id", customer.Id);
+                command.Parameters.AddWithValue("@FirstName", customer.FirstName);
+                command.Parameters.AddWithValue("@Mobilenumber", customer.Mobilenumber);
+                command.Parameters.AddWithValue("@Licence", customer.Licence);
+                command.Parameters.AddWithValue("@Nic", customer.Nic);
+                command.Parameters.AddWithValue("@Password", customer.Password);
+                command.Parameters.AddWithValue("@IsActive", customer.IsActive);
+
+                await command.ExecuteNonQueryAsync();
+
+
+                return customer;
+            }
+        }
+
     }
 }
--- BackEnd/BIKERENTALAPI/BIKERENTALAPI/obj/Debug/net8.0/BIKERENTALAPI.AssemblyInfo.cs
+++ BackEnd/BIKERENTALAPI/BIKERENTALAPI/obj/Debug/net8.0/BIKERENTALAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BIKERENTALAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+805e38327a873986b685dd2ef9db93cf34dd344f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BIKERENTALAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BIKERENTALAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
